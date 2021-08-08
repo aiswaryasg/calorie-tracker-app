539d2b022766c8a9a128d52120ef33fc6f2cd882
Aiswarya.S.G <88581896+aiswaryasg@users.noreply.github.com>
2021-08-08 12:28:58 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,22 +1,14 @@
-"""calorie_app URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path
+from .views import
+HomePageView,LoginPage,LogOutPage,select_food,add_food,RegisterPage,ProfilePage,update_food,delete_food
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('calories.urls')),
+path('', HomePageView,name='home'),
+path('login/',LoginPage,name='login'),
+path('logout/',LogOutPage,name='logout'),
+path('select_food/',select_food,name='select_food'),
+path('add_food/',add_food,name='add_food'),
+path('update_food/<str:pk>/',update_food,name='update_food'),
+path('delete_food/<str:pk>/',delete_food,name='delete_food'),
+path('register/',RegisterPage,name='register'),
+path('profile/',ProfilePage,name='profile'),
 ]
